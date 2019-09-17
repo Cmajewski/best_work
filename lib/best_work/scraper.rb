c01a7382f6865733ff9accf03a1f96e06f6f13f3
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-17 12:47:59 +0000

    startup accessors

--- lib/best_work/scraper.rb
+++ lib/best_work/scraper.rb
@@ -5,18 +5,15 @@ module BestWork
       Nokogiri::HTML(open("https://www.builtinnyc.com/companies/best-places-to-work-nyc"))
     end
 
-    def scrape_name
-      x=[]
-      y=[]
-      type=[]
-      a=[]
-      b=[]
+    def scrape_companies
+
       self.get_file.css("div.company--best-place").each  do |company|
-        x<<company.css("h2 a").text
-        y<<company.css("div.field-about-us div.item").text.strip
-        type<<company.css("div.field-type").text.strip
-        a<<company.css("time.datetime").text
-        b<<company.css("div.field.field-location  div.item").text
+
+        company.css("h2 a").text
+        company.css("div.field-about-us div.item").text.strip
+        company.css("div.field-type").text.strip
+        company.css("time.datetime").text
+        company.css("div.field.field-location  div.item").text
       end
       binding.pry
     end
--- lib/best_work/startups.rb
+++ lib/best_work/startups.rb
@@ -1,11 +1,26 @@
 module BestWork
   class Startups
-    attr_accessor :name, :description, :plane_ticket, :hotel_rate
+    attr_accessor :name, :business_type, :description, :founded, :location
     @@all=[]
 
-    def initialize
+    def initialize (name=nil,business_type=nil,description=nil,founded=nil,location=nil)
+      @name=name
+      @business_type=business_type
+      @description=description
+      @founded=founded
+      @location=location
+      @@all<<self
     end
 
+    def self.create_from_scraper (path)
+      self.new(
+      path.css("h2 a").text
+      path.css("div.field-about-us div.item").text.strip
+      path.css("div.field-type").text.strip
+      path.css("time.datetime").text
+      path.css("div.field.field-location  div.item").text
+      )
+    end
     def self.all
       puts "[maine, RI, texas, cali]"
 
